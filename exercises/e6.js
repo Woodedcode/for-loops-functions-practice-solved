a5f5f9a979e751c8948d69a330a1648170d7d8e7
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-08-21 13:08:45 -0500

    Update e6.js

--- exercises/e6.js
+++ exercises/e6.js
@@ -4,10 +4,13 @@
 // getClientWithNoMoney(bankAccounts) => ['Kevin', 'Jon']
 
 export function getClientWithNoMoney(array) {
-  // Your code goes here...
-  return array
-  .filter(account => account.balance === 0)
-  .map(account => account.name)
+  let result = []
+
+  for (let i = 0; i < array.length; i++)
+    if (array[i].balance === 0) {
+      result.push(array[i].name)
+    }
+    return result
 }
 
 // === TEST YOURSELF ===
