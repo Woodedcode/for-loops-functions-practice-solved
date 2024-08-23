2107c2189f987db9c13ece1bd7db1196cf622f3a
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-08-23 10:53:08 -0500

    Update e16.js

--- exercises/e16.js
+++ exercises/e16.js
@@ -5,17 +5,20 @@
 // flatArrays([['d', 'r'], 'z', 'b', ['f', 'y']]) => ['d', 'r', 'z', 'b', 'f', 'y']
 // NOTE: You can NOT use the array.flat() method in your code
 
-export function doesArrayInclude(array, value) {
-  let found = false;
+export function flatArrays(array) {
+  let flatArray = [];
 
   for (let item of array) {
-    if (item === value) {
-      found = true;
-      break;
+    if (Array.isArray(item)) {
+      for (let subItem of item) {
+        flatArray.push(subItem);
+      }
+    } else {
+      flatArray.push(item);
     }
   }
 
-  return found;
+  return flatArray;
 }
 
 
